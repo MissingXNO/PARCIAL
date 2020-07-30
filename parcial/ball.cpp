dcd0cb76123bce0b17410d56fe040e3571044172
MissingXNO <santiago.giraldot@udea.edu.co>
2020-07-30 09:46:57 -0500

    SE AGREGÓ CAÍDA LIBRE EN VERTICAL PARA EL CUERPO

--- parcial/ball.cpp
+++ parcial/ball.cpp
@@ -3,17 +3,26 @@
 
 ball::ball(QGraphicsItem *parent)
 {
+    QTimer *movtimer = new QTimer();
+    connect(movtimer,SIGNAL(timeout()),this,SLOT(move()));
+    movtimer->start(5);
 
+    QTimer *acelerate = new QTimer();
+    connect(acelerate,SIGNAL(timeout()),this,SLOT(change_speed()));
+    acelerate->start(10);
 }
 
 void ball::move()
 {
-
+    setPos(x(),y()+v);
 }
 
 void ball::change_speed()
 {
-
+    if(v<=vf)
+    {
+        v+=g;
+    }
 }
 
 QRectF ball::boundingRect() const
